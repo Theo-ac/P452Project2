cac97f210e4a125ea25b542e969f48a77685756f
Theo-ac <125707117+Theo-ac@users.noreply.github.com>
2026-04-29 22:53:28 -0500

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -5,4 +5,9 @@ import matplotlib.pyplot as plt
 from qiskit.quantum_info import Statevector
 from qiskit.visualization import plot_histogram
 st.title("Theo's Density Profile Calculator")
-gb = st.text_input("What's your g_B?", value = 0)
+gb = st.text_input("Enter a value for g_B", value = 0.0)
+gbf = st.text_input("Enter a value for g_BF", value = 0.0)
+wb = st.text_input("Enter a value for w_B", value = 0.0)
+wf = st.text_input("Enter a value for w_F", value = 0.0)
+mb = st.text_input("Enter a value for m_B", value = 0.0)
+mf = st.text_input("Enter a value for m_F", value = 0.0)
