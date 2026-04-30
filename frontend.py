be7f028b34eee4a8357759c81c008b0a55ba2c98
Theo-ac <125707117+Theo-ac@users.noreply.github.com>
2026-04-29 22:47:18 -0500

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,1 +1,7 @@
-
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+#from backend import
+from qiskit.quantum_info import Statevector
+from qiskit.visualization import plot_histogram
+st.title("Theo's Density Profile Calculator")
